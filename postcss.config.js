4e269f3308a07eacc5e20b0fcd45f6645d8c3511
chen <1072733116@qq.com>
2024-03-02 18:30:24 +0800

    update

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    'postcss-pxtorem': {
+      rootValue: 16,
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0
+    }
+  }
+};
